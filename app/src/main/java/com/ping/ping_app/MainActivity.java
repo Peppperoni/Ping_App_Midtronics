0e1abc381c8befeb5cf1bc5f99e116344a70231b
liu00019 <liu00019@rangers.uwp.edu>
2019-09-07 01:47:44 -0500

    Navigation bar updated. Country Detail Activity updated(country:China).
    Todo:
    1. Update my profile
    2. Add the list of countries
    3. New Intent
    4. Polish UI

---
+++ app/src/main/java/com/ping/ping_app/MainActivity.java
@@ -0,0 +1,49 @@
+package com.ping.ping_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_me:
+                    mTextMessage.setText(R.string.title_country);
+                    return true;
+                case R.id.navigation_country:
+                    toShowCountryActivity();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+
+    }
+
+    void toShowCountryActivity (){
+        Intent intent = new Intent(this, ShowCountryActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/ping/ping_app/ShowCountryActivity.java
@@ -0,0 +1,141 @@
+package com.ping.ping_app;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class ShowCountryActivity extends AppCompatActivity {
+
+    TextView mTextView;
+    String error = ""; // string field
+    String countryURL = "https://restcountries.eu/rest/v1/name/";
+    String country = "China";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_country);
+
+        mTextView = (TextView)findViewById(R.id.listTextView);
+        new HttpGetTask().execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemSelected = item.getItemId();
+        if (menuItemSelected == R.id.action_search){
+            Context context = ShowCountryActivity.this;
+            String message = "Search clicked";
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private String getDataFromUrl(String CountryUrl) {
+
+        String result = null;
+        int resCode;
+        InputStream in;
+        try {
+            URL url = new URL(CountryUrl);
+            URLConnection urlConn = url.openConnection();
+
+            HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
+            httpsConn.setAllowUserInteraction(false);
+            httpsConn.setInstanceFollowRedirects(true);
+            httpsConn.setRequestMethod("GET");
+            httpsConn.connect();
+            resCode = httpsConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpsConn.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        in, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                in.close();
+                result = sb.toString();
+            } else {
+                error += resCode;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private void updateUI(String js){
+        try{
+            JSONObject country = new JSONObject(js.substring(1, js.length()-1));
+            //country, capital, population, area, region, sub-region
+            String name = country.getString("name");
+            String capital = country.getString("capital");
+            String population = country.getString("population");
+            String area = country.getString("area");
+            String region = country.getString("region");
+            String subregion = country.getString("subregion");
+
+            mTextView.setText(
+                    "name:                      " + name + "\n"
+                    +"capital                     " + capital + "\n"
+                    +"population                    " + population + "\n"
+                    +"area                     " + area + "\n"
+                    +"region                    " + region + "\n"
+                    +"subregion                       " + subregion
+            );
+        }catch(Exception e){
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public class HttpGetTask extends AsyncTask<URL, Void, String>{
+        @Override
+        protected String doInBackground(URL... urls) {
+            return getDataFromUrl(countryURL+country);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+                if (s != null && !s.equals("")) {
+                    /////////Need to pass country name here
+                    updateUI(s);
+                }
+                else{
+                    mTextView.setText("Country doesn't exist");
+                }
+        }
+    }
+}
